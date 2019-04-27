b24742c733e0d4e3fbde287d407473f42a62e1be
Glitch (benim-botum-lan) <none>
2019-04-27 10:29:06 +0000

    🎊⏰ Checkpoint ./komutlar/help.js:2558838/643

---
+++ komutlar/help.js
@@ -0,0 +1,25 @@
+               const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+exports.run = (client, message, params) => {
+      if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0xff0000)
+    .setDescription(':white_check_mark: Komutlar İçin ** https://www.volcanobot.com **');
+    message.channel.sendEmbed(ozelmesajkontrol) }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['yardım'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'komut',
+  description: 'Tüm komutları gösterir.',
+  usage: 'yardım',
+};
