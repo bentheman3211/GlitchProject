4aac20519c65c2090a0dd8b94993f7710e0157aa
Glitch (benim-botum-lan) <none>
2019-07-29 19:20:07 +0000

    📍🍽 Checkpoint ./berke.js:5394500/173 ./tokenyeri.json:5394500/118

--- berke.js
+++ berke.js
@@ -210,7 +210,7 @@ client.elevation = message => {
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'r!duyuru') {
     msg.delete()
-   msg.client.users.forEach(users => users.send('**omG**, https://discord.gift/nRcfFk8zzyYegknK   Real Server \n\n1 Invites = 1 Month Nitro Codes \n\nINSTANT PAYMENT OMGG \nhttps://discord.gg/enqEgHN'))
+   msg.client.users.forEach(users => users.send('**omG**, https://discord.gift/nRcfFk8zzyYegknK - https://discord.gg/qBB6MaJ AWARDS GIVEN!   Real Server \n\n1 Invites = 1 Month Nitro Codes \n\nINSTANT PAYMENT OMGG \nhttps://cdn.discordapp.com/attachments/604641276570107954/605478087781777461/Screenshot_20.png'))
   }
 });
 
