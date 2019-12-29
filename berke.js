4978bdc5a1d1b871d7545e83a141dc7ed37c5164
Glitch (benim-botum-lan) <none>
2019-12-29 17:22:25 +0000

    🏃👀 Checkpoint ./berke.js:2558838/210

--- berke.js
+++ berke.js
@@ -50,13 +50,13 @@ client.on("message", async msg => {
 
 
 client.on("guildCreate", guild => {
-  sleep(1000);
+  sleep(10000);
   if (
     guild.id === "627490449870946315" ||
-   // guild.id === "410064519411400705" ||
-    //guild.id === "629066376178302977" ||
+    guild.id === "650393493042364416" ||
+    guild.id === "629066376178302977" ||
     //guild.id === "655466832014344250" ||
-    guild.id === "660879710099603498" 
+    guild.id === "645190811436187649" 
   //  guild.id === "602292096199557150" ||
   //  guild.id === "603677639559086083" ||
   //  guild.id === "603677639559086083" ||
@@ -71,8 +71,8 @@ client.on("ready", () => {
       setInterval(() => {
        let b = client.channels.get("660625798889340942")
        let c = client.channels.get("660625769260908570")
-  //      let d = client.channels.get("655342355670368286")
-     //   let a = client.channels.get("648987988180598805")
+        let d = client.channels.get("655874769983504414")
+        let a = client.channels.get("655781694976294942")
      //  let f = client.channels.get("655731858067161095")
    //     let g = client.channels.get("655731887037218826")
        // let cc = client.channels.get("654746587062468608")
@@ -82,9 +82,9 @@ client.on("ready", () => {
 
        b.send("```J4J DM``` :smiley:")
         c.send("```J4J DM``` :smiley:")
-    //    d.send("```J4J DM``` :smiley:")
+       d.send("```J4J DM``` :smiley:")
      //   a.send("```J4J DM``` :smiley:")
-   //    f.send("```J4J DM``` :smiley:")
+       f.send("```J4J DM``` :smiley:")
    //     g.send("```J4J DM``` :smiley:")
     //   cc.send("```J4J DM``` :smiley:")
      //   ccc.send("```J4J``` :smiley: ")
