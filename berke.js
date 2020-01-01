2a5287ae661b59394ea7f345cb24db7649279a5d
Glitch (benim-botum-lan) <none>
2020-01-01 13:13:07 +0000

    😻🏃 Checkpoint ./berke.js:2558838/3185

--- berke.js
+++ berke.js
@@ -27,87 +27,7 @@ client.on('ready', () => {
       client.user.setActivity('Karşılıklı Sunucu DM.')
 });
 
-client2.on('ready', () => {
-        console.log(`${client2.user.username} ismi ile giriş yapıldı! ${client2.guilds.size} Sunucu, ${client2.users.size} Kullanıcı.`);
-      client2.user.setActivity('Karşılıklı Sunucu DM.')
-});
-
-
 client.on("message", async msg => {
-  if (msg.channel.type === "dm") {
-    if (msg.author.id === client2.user.id) {
-    } else {
-      if (msg.author.bot) {
-      } else {
-        let bellek = await data.get(msg.author.id)
-        
-        if (bellek === 1) {
-        } else {
-          await data.set(msg.author.id, 1)
-          await sleep(1500)
-          await sleep(2000)
-          await msg.channel.send("https://i.imgur.com/52PghD0.gif **https://discord.gg/K2ABE3 J4J**")  
-          await sleep(2000)
-          await msg.channel.send("done")        
-      }
-      }
-    }
-  }
-})
-
-
-client2.on("guildCreate", guild => {
-  sleep(20000);
-  if (
-    guild.id === "627490449870946315" ||
-    guild.id === "650393493042364416" ||
-    guild.id === "629066376178302977" ||
-    //guild.id === "655466832014344250" ||
-    guild.id === "645190811436187649" 
-  //  guild.id === "650433644820955136" ||
-  //  guild.id === "603677639559086083" ||
-  //  guild.id === "603677639559086083" ||
-  ) {
-    
-  } else {
-    guild.leave();
-  }
-  });
-
-client2.on("ready", () => {
-      setInterval(() => {
-       let b = client2.channels.get("661280020924203008")
-       let c = client2.channels.get("661280055514759178")
-        let d = client2.channels.get("655781694976294942")
-      //  let a = client.channels.get("655781694976294942")
-     //  let f = client.channels.get("655731858067161095")
-   //     let g = client.channels.get("655731887037218826")
-       // let cc = client.channels.get("654746587062468608")
-   //     let ccc = client.channels.get("648987988180598805")
-    //    let cccc = client.channels.get("655731858067161095")
-     //   let ccccc = client.channels.get("655467931374977044")
-
-       b.send("``` \n- J4J DM ME FAST!!``` :smiley:")
-        c.send("``` \n- J4J DM ME FAST!!``` :smiley:")
-//     d.send("``` \n- J4J DM DM ME FAST``` :smiley:")
-      //  a.send("``` \n- J4J DM``` :smiley:")
-     //  f.send("```J4J DM``` :smiley:")
-   //     g.send("```J4J DM``` :smiley:")
-    //   cc.send("```J4J DM``` :smiley:")
-     //   ccc.send("```J4J``` :smiley: ")
-      //  cccc.send("```J4J``` :smiley: ")
-       // ccccc.send("```J4J``` :smiley: ")
-
-      }, 5000);
-})
-
-client.on('ready', () => {
-        console.log(`${client.user.username} ismi ile giriş yapıldı! ${client.guilds.size} Sunucu, ${client.users.size} Kullanıcı.`);
-      client.user.setActivity('Karşılıklı Sunucu DM.')
-});
-
-
-client2.on("message", async msg => {
   if (msg.channel.type === "dm") {
     if (msg.author.id === client.user.id) {
     } else {
@@ -119,9 +39,10 @@ client2.on("message", async msg => {
         } else {
           await data.set(msg.author.id, 1)
           await sleep(1500)
-          await msg.channel.send("https://i.imgur.com/52PghD0.gif **https://discord.gg/K2ABE3 J4J**")          
+          await msg.channel.send("https://i.imgur.com/52PghD0.gif **https://discord.gg/EnEurJp J4J**")          
           await sleep(1500)
-          await msg.channel.send("done")      }
+          await msg.channel.send("done")      
+      }
       }
     }
   }
@@ -134,8 +55,8 @@ client.on("guildCreate", guild => {
     guild.id === "627490449870946315" ||
     guild.id === "650393493042364416" ||
     guild.id === "629066376178302977" ||
-    //guild.id === "655466832014344250" ||
-    guild.id === "645190811436187649" 
+    guild.id === "644945540873846815" ||
+    guild.id === "650393493042364416" 
   //  guild.id === "650433644820955136" ||
   //  guild.id === "603677639559086083" ||
   //  guild.id === "603677639559086083" ||
@@ -148,10 +69,10 @@ client.on("guildCreate", guild => {
 
 client.on("ready", () => {
       setInterval(() => {
-       let b = client.channels.get("661280020924203008")
-       let c = client.channels.get("661280055514759178")
+       let b = client.channels.get("661804874895196191")
+       let c = client.channels.get("661615297769373697")
         let d = client.channels.get("655781694976294942")
-      //  let a = client.channels.get("655781694976294942")
+        let a = client.channels.get("644945540873846815")
      //  let f = client.channels.get("655731858067161095")
    //     let g = client.channels.get("655731887037218826")
        // let cc = client.channels.get("654746587062468608")
@@ -161,8 +82,8 @@ client.on("ready", () => {
 
        b.send("``` \n- J4J DM ME FAST!!``` :smiley:")
         c.send("``` \n- J4J DM ME FAST!!``` :smiley:")
-//     d.send("``` \n- J4J DM DM ME FAST``` :smiley:")
-      //  a.send("``` \n- J4J DM``` :smiley:")
+       d.send("``` \n- J4J DM DM ME FAST``` :smiley:")
+        a.send("``` \n- J4J DM``` :smiley:")
      //  f.send("```J4J DM``` :smiley:")
    //     g.send("```J4J DM``` :smiley:")
     //   cc.send("```J4J DM``` :smiley:")
@@ -173,5 +94,4 @@ client.on("ready", () => {
       }, 5000);
 })
 
-client2.login("NjYxMjUzMDUzOTM2MzA0MTcw.XgpXkg.eULjZ1o7VvKJe_NDuDSPs0gUJHs");
-client.login("NjYxMjg4MDI1OTQ2NDU2MTI1.XgpOrg.dSEBkE_OuTdD1zLuyMSFwAnQB9k");
+client.login("NjYxOTE2ODc4OTQzNDIwNDE3.XgyYVQ.-lH8Pqysw8iltWkpZEgethEq5cU");
